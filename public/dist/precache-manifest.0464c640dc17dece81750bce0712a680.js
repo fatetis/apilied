3aea38e58f76b5a2943f180e2e3e5616492437cc
何杨坤 <you@example.com>
2021-06-30 13:44:11 +0800

    打包前端文件 访问路径http://api.lied.com/dist/index.html

---
+++ public/dist/precache-manifest.0464c640dc17dece81750bce0712a680.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1213c1e4ec089217327e6dd79ec6e71c",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "4d2a1374dddc3945f517fce7e5e44279",
+    "url": "/dist/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/dist/robots.txt"
+  },
+  {
+    "revision": "c420db031dc22d534fcd",
+    "url": "/dist/static/css/app.8b6e8537.css"
+  },
+  {
+    "revision": "6c5cca032012cc1519cd",
+    "url": "/dist/static/css/chunk-vendors.22e46f21.css"
+  },
+  {
+    "revision": "af1bf108aba913d573fb68b941ed1e9e",
+    "url": "/dist/static/img/alipay.af1bf108.png"
+  },
+  {
+    "revision": "a744338e44f114f20812330149c8481e",
+    "url": "/dist/static/img/balance.a744338e.png"
+  },
+  {
+    "revision": "e1f7cf52f3c30e88113c24a44fdfdc13",
+    "url": "/dist/static/img/bg.e1f7cf52.png"
+  },
+  {
+    "revision": "be589b80938ea4ea482fa83132e230a0",
+    "url": "/dist/static/img/cart_shop.be589b80.png"
+  },
+  {
+    "revision": "00962156d986c5398a910160630a9f50",
+    "url": "/dist/static/img/eye_open.00962156.png"
+  },
+  {
+    "revision": "0d878031079ffe222bc55feb9f689c62",
+    "url": "/dist/static/img/fatetis.0d878031.jpg"
+  },
+  {
+    "revision": "4955e5213c7a351c08f2f4c4b3cddde8",
+    "url": "/dist/static/img/index.4955e521.jpg"
+  },
+  {
+    "revision": "15a205fa7a6cb0feef7e9e513856e4ac",
+    "url": "/dist/static/img/null-page-draw.15a205fa.png"
+  },
+  {
+    "revision": "7628f31095318badc1ea7e243d9c02a4",
+    "url": "/dist/static/img/prod_customer_service.7628f310.png"
+  },
+  {
+    "revision": "923fd10ada9c0361b013eec81cc79113",
+    "url": "/dist/static/img/prod_seller.923fd10a.png"
+  },
+  {
+    "revision": "4fb6d91ef397f20852beb619523d2b70",
+    "url": "/dist/static/img/product_1.4fb6d91e.jpg"
+  },
+  {
+    "revision": "1fe02613fb34437d79571d8e924eff53",
+    "url": "/dist/static/img/product_2.1fe02613.jpg"
+  },
+  {
+    "revision": "6d2b1de0f3e5bed904221dcb9bb9eedb",
+    "url": "/dist/static/img/recommand.6d2b1de0.png"
+  },
+  {
+    "revision": "8c335e2ed807fdf45f8761525271b01f",
+    "url": "/dist/static/img/setting.8c335e2e.png"
+  },
+  {
+    "revision": "e0f8b061c366dcf1241fc5fb4e615728",
+    "url": "/dist/static/img/small_letter.e0f8b061.png"
+  },
+  {
+    "revision": "b161040af6f185cfe3f11814c8e149b4",
+    "url": "/dist/static/img/stroll_day_index.b161040a.png"
+  },
+  {
+    "revision": "1ff874d08c092119d027620cc8e7b74d",
+    "url": "/dist/static/img/wait_refund.1ff874d0.png"
+  },
+  {
+    "revision": "f2eca5bee0b5e4571b0bbd1a44dbfbe9",
+    "url": "/dist/static/img/wechat_pay.f2eca5be.png"
+  },
+  {
+    "revision": "914057b6136f4d3f1cf0",
+    "url": "/dist/static/js/about.b84c3787.js"
+  },
+  {
+    "revision": "c420db031dc22d534fcd",
+    "url": "/dist/static/js/app.f27d0866.js"
+  },
+  {
+    "revision": "6c5cca032012cc1519cd",
+    "url": "/dist/static/js/chunk-vendors.e434ca8f.js"
+  },
+  {
+    "revision": "827569e88f4f0f17fa0e17c30cb5bc5c",
+    "url": "/dist/statics/mock/city.json"
+  }
+]);
